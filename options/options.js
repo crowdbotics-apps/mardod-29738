b306e08a5c640db0631b2692b18d0d3e93be7170
Crowdbotics <team@crowdbotics.com>
2021-08-12 01:10:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mardod_29738Identifier",
+  url: "https://mardod_29738Identifier.botics.co",
+  api: "https://mardod_29738Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
